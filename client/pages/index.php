b88dc14ee388596517578d100fdaf076f732019d
Lacikosz <lacina.kuba15@seznam.cz>
2025-03-31 22:05:19 +0200

    added structure

---
+++ client/pages/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        include '../partials/head.php';
+    ?>
+</head>
+<body>
+    <?php include '../partials/header.php'; ?>
+    <main>
+
+    </main>
+</body>
+<footer>
+        <?php
+            include '../partials/head.php';
+        ?>
+</footer>
+</html>
