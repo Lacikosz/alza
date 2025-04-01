aa10a684cca9902e9f70fb0f226fd9148f4a7faf
Lacikosz <lacina.kuba15@seznam.cz>
2025-04-01 15:21:18 +0200

    editing js

--- client/pages/index.php
+++ client/pages/index.php
@@ -8,10 +8,22 @@
 <body>
     <?php include '../partials/header.php'; ?>
     <main>
+        <div id="myDIV" class="header">
+            <h2>My To Do List</h2>
+            <input type="text" id="myInput" placeholder="ToDo..">
+            <span onclick="newElement()" class="addBtn">Přidat</span>
+        </div>
+        <ul id="myUL" >
+            <li>Jit do gymu</li>
+            <li>Učit se</li>
+            <li>Uklidit</li>
+            <li class="checked">Pracovat</li>
+        </ul>
 
     </main>
 </body>
 <footer>
+    <script src="../js/script.js"></script>
         <?php
             include '../partials/head.php';
         ?>
